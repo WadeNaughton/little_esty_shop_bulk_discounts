e8133f7e1a92ce4c2d8a16286d99ba8c6e9c1851
Wade Naughton <wadenaughton@gmail.com>
2022-01-18 14:01:21 -0500

    inovice model test added

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -34,9 +34,31 @@ RSpec.describe InvoiceItem, type: :model do
       @ii_2 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_2.id, quantity: 1, unit_price: 8, status: 0)
       @ii_3 = InvoiceItem.create!(invoice_id: @i2.id, item_id: @item_3.id, quantity: 1, unit_price: 5, status: 2)
       @ii_4 = InvoiceItem.create!(invoice_id: @i3.id, item_id: @item_3.id, quantity: 1, unit_price: 5, status: 1)
+
     end
     it 'incomplete_invoices' do
       expect(InvoiceItem.incomplete_invoices).to eq([@i1, @i3])
+    end
+    it "finds discount" do
+      merchant = Merchant.create!(name: 'Hair Care')
+
+      item1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: merchant.id)
+      item2 = Item.create!(name: "Conditioner", description: "This makes your hair shiny", unit_price: 8, merchant_id: merchant.id)
+      item3 = Item.create!(name: "Brush", description: "This takes out tangles", unit_price: 5, merchant_id: merchant.id)
+
+      customer1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
+
+      invoice1 = Invoice.create!(customer_id: customer1.id, status: 2, created_at: "2012-03-27 14:54:09")
+
+      ii1 = InvoiceItem.create!(invoice_id: invoice1.id, item_id: item1.id, quantity: 8, unit_price: 10, status: 2)
+      ii3 = InvoiceItem.create!(invoice_id: invoice1.id, item_id: item2.id, quantity: 2, unit_price: 8, status: 2)
+      ii4 = InvoiceItem.create!(invoice_id: invoice1.id, item_id: item3.id, quantity: 1, unit_price: 5, status: 2)
+
+      transaction1 = Transaction.create!(credit_card_number: 203942, result: 1, invoice_id: invoice1.id)
+
+      discount1 = merchant.discounts.create!(name: 'discount1', percent_discount: 10, quantity_limit: 6)
+      discount2 = merchant.discounts.create!(name: 'discount2', percent_discount: 25, quantity_limit: 4)
+      expect(ii1.discount_find(ii1.id)).to eq(discount1)
     end
   end
 end
